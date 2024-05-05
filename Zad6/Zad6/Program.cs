465aa1167216decfb3ccfb44971bd74f5ce1d6c9
Hubszer <s25160@pjwstk.edu.pl>
2024-05-05 20:00:52 +0200

    almost

---
+++ Zad6/Zad6/Program.cs
@@ -0,0 +1,30 @@
+using Zad6;
+using Zad6.Repo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IWarehousesRepository, WarehouseRepo>();
+builder.Services.AddScoped<IWarehouseRepository2,WarehouseRepo2>();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
